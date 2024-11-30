284767aa4867e472b116a56154cb8df5ee18f6a8
Mayara <mayara.wojceikovski@gmail.com>
2024-11-30 13:49:31 -0300

    codigo prova

---
+++ API/my-app/src/Componentes/ListaPlanta.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { Planta } from '../Interface/Planta';
+
+const ListaPlanta: React.FC = () => {
+    const [planta, setPlanta] = useState<Planta[]>([]);
+
+    useEffect(() => {
+        fetch('<https://api.exemplo.com/>')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro na requisição: ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setPlanta(data);
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Lista de Plantas</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Criado Em</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {planta.map(planta => (
+                        <tr key={planta.id}>
+                            <td>{planta.id}</td>
+                            <td>{planta.nome}</td>
+                            <td>{planta.descricao}</td>
+                            <td>{new Date(planta.criadoEm).toLocaleDateString()}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ListaPlanta;
---
+++ API/my-app/src/Interface/Planta.tsx
@@ -0,0 +1,6 @@
+export interface Planta {
+    id: number;
+    nome: string;
+    descricao: string;
+    criadoEm: Date;
+}
