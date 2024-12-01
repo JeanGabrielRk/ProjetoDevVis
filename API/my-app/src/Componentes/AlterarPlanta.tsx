767a75c7bd01592d9abc83dc8095a04185285975
Mayara <mayara.wojceikovski@gmail.com>
2024-12-01 15:26:09 -0300

    alteração codigo

--- API/my-app/src/Componentes/AlterarPlanta.tsx
+++ API/my-app/src/Componentes/AlterarPlanta.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
 import { Planta } from '../Interfaces/Planta';
 
 const AlterarPlanta: React.FC = () => {
